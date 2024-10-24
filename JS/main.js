3de777a427d93d9d1d61ca24312a704c11c484e8
Ashish Khadka <aasheeshkhadka@gmail.com>
2024-10-24 21:59:22 +0545

    added about page

--- JS/main.js
+++ JS/main.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
 
 /*------------------ Typed JS ---------------------*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Web Designer', 'Student'],
+    strings: ['Frontend Developer', 'Student', 'SEO Executive'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
